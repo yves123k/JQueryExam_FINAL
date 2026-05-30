c29a83445baa36a88a75c5b64329cb5fcc6b3358
yves123k <Kymbelominmarius@gmail.com>
2026-05-30 19:04:32 +0000

    mise a jours interface

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 // app.js
 
+
+
+/* CODE POPUP */
+$(document).ready(function () {
+
+  // Ouvrir le popup quand on clique sur modifier
+  $(".action-btn.edit").on("click", function () {
+    $("#popup-modifier-note").addClass("active");
+    $("#nouvelle-note").focus();
+  });
+
+  // Fermer avec le bouton X
+  $("#fermer-popup").on("click", function () {
+    $("#popup-modifier-note").removeClass("active");
+  });
+
+  // Fermer avec Annuler
+  $("#annuler-popup").on("click", function () {
+    $("#popup-modifier-note").removeClass("active");
+  });
+
+  // Fermer en cliquant sur le fond noir
+  $("#popup-modifier-note").on("click", function (e) {
+    if (e.target === this) {
+      $(this).removeClass("active");
+    }
+  });
+
+});
